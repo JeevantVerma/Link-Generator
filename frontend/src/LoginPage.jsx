958462253a1e669deb320d726de4dd3d48cb0b2e
Jeevant Verma <151034115+JeevantVerma@users.noreply.github.com>
2024-07-20 00:33:29 +0530

    Update LoginPage.jsx

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,76 +1,138 @@
 import { useState } from "react";
-import {
-  Grid,
-  Typography,
-  TextField,
-  Button,
-} from "@mui/material";
+import { Grid, Container, Typography, TextField, Button, Snackbar, Alert } from '@mui/material';
 
 const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [snackbarOpen, setSnackbarOpen] = useState('');
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [error, setError] = useState('');
 
-  const handleLogin = () => {
-    // Validate email and password
-    if (!email || !password) {
-      setError("Email and password are required.");
-      return;
+  const handleLogin = async (e) => {
+    
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailRegex.test(email)) {
+    setError("Invalid email address");
+    return;
     }
-
-    // Perform login logic
-    // Assuming API call or further validation
-
-    // Clear error state after successful login
     setError("");
+    
+    dispatch(setIsFetching());
+    const config = {
+      method: 'GET',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:4000/login',
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin":
+          "https://generate.mlsctiet.com, http://localhost:5173",
+        "email" : email,
+        "password" : password 
+      },
+      withCredentials: true,
+    };
+
+    try {
+      const response = await axios.request(config);
+      console.log(response.data);
+      if (response.status === 200) {
+        console.log('Login success');
+        const redirectUrl = response.data.redirectUrl;
+        dispatch(loginSuccess({"email":email}));
+
+        if (redirectUrl) {
+          console.log('redirect');
+          // window.location.href = redirectUrl;
+          navigate(redirectUrl);
+        } else {
+          console.log('No redirect URL provided by the backend');
+        }
+      } else {
+        dispatch(loginFailure());
+        console.log('Login failed');
+        console.log(e);
+        setSnackbarOpen(true);
+      }
+    } catch (error) {
+      dispatch(loginFailure());
+      console.error('Error during login:', error.message);
+      console.error('Full error:', error);
+      setSnackbarOpen(true);
+    }
   };
 
   return (
-    <Grid
-      container
-      spacing={2}
-      alignItems="center"
-      justifyContent="center"
-      sx={{ backgroundColor: "#eaeff1", padding: { xs: 3.8, md: 5 }, borderRadius: 10 }}
+    <Container
+      maxWidth="md" 
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '89vh',
+        width: {
+          xs: '100%',
+          md: '50vw',
+        }
+      }}
     >
-      <Typography variant="h4" align="center">Login using Username</Typography>
-
-      <Grid item xs={12} sx={{ pt: { xs: 32, md: 16 }, pl: 16 }}>
-        <TextField
-          label="Enter your email"
-          variant="outlined"
-          fullWidth
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          error={!!error}
-          helperText={error}
-        />
-      </Grid>
-
-      <Grid item xs={12} sx={{ pt: { xs: 32, md: 16 }, pl: 16 }}>
-        <TextField
-          label="Enter your password"
-          variant="outlined"
-          fullWidth
-          value={password}
-          required
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </Grid>
-      <Grid item xs={12} sm={4}>
-      <Button
-        type="submit"
-        fullWidth
+      <Grid
+        container
         spacing={2}
-        variant="contained"
-        color="primary"
-        onClick={handleLogin}
+        alignItems="center"
+        justifyContent="center"
+        sx={{ backgroundColor: '#eaeff1', padding: { xs: 3.8, md: 5 }, borderRadius: 10 }}
       >
-        Login
-      </Button>
+        <Typography variant="h4" align="center">Login</Typography>
+
+        <Grid item xs={12} sx={{ pt: { xs: 32, md: 16 }, pl: 16 }}>
+          <TextField
+            label="Enter your Email"
+            variant="outlined"
+            fullWidth
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </Grid>
+
+        <Grid item xs={12} sx={{ pt: { xs: 32, md: 16 }, pl: 16 }}>
+          <TextField
+            label="Enter your password"
+            variant="outlined"
+            fullWidth
+            value={password}
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Button
+            // type="submit"
+            fullWidth
+            spacing={2}
+            variant="contained"
+            color="primary"
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+        </Grid>
+        <Snackbar 
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={() => setSnackbarOpen(false)}>
+          <Alert
+            onClose={() => setSnackbarOpen(false)}
+            severity="error"
+            sx={{ width: '100%' }}>
+            Email or password is invalid
+          </Alert>
+        </Snackbar>
       </Grid>
-    </Grid>
+    </Container>
   );
 };
 
