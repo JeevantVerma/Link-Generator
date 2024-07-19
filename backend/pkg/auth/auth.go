ded60ce7169d01686e45ad28356ff89a9eb5929e
Preetinder Singh <preetindersingh13per@gmail.com>
2024-07-19 10:33:14 +0530

    auth updated

--- backend/pkg/auth/auth.go
+++ backend/pkg/auth/auth.go
@@ -147,7 +147,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	url := "http://localhost:5173/Adminpage"
+	url := "/Adminpage"
 	fmt.Printf("Route Url: " + url)
 	json.NewEncoder(w).Encode(Response{Status: "success", RedirectUrl: url})
 }
