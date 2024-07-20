2a5d02a5c80f098658ca1a936644a2c1686ff7f9
Preetinder Singh <preetindersingh13per@gmail.com>
2024-07-20 11:58:16 +0530

    Add joining time/date in backend

--- backend/pkg/admin/admin.go
+++ backend/pkg/admin/admin.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
 
 	_ "github.com/lib/pq"
 	"golang.org/x/crypto/bcrypt"
@@ -16,10 +17,10 @@ import (
 type admindetail struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+	JoinDate string `json:"joinDate"`
 }
 
 var db *sql.DB
-var conn string
 
 func SetDBConnection(dbSql *sql.DB) {
 	db = dbSql
@@ -44,9 +45,14 @@ func AddAdmin(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 
-	query := `INSERT INTO admin (email, password) VALUES ($1, $2)`
+	query := `INSERT INTO admin (email, password, joinDate) VALUES ($1, $2, $3)`
 	password, _ := bcrypt.GenerateFromPassword([]byte(req.Password), 8)
-	_, err = db.ExecContext(ctx, query, req.Email, password)
+
+	currentTime := time.Now()
+	joinDate := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, currentTime.Location())
+	joinDateString := joinDate.Format("2006-01-02")
+
+	_, err = db.ExecContext(ctx, query, req.Email, password, joinDateString)
 	if err != nil {
 		log.Printf("Error executing query: %v", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -56,4 +62,43 @@ func AddAdmin(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(req)
 	w.Write([]byte("Admin Added"))
+}
+
+func GetAllAdmins(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	query := `SELECT email, joinDate FROM admin`
+
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		log.Printf("Error executing query: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var admins []admindetail
+	for rows.Next() {
+		var admin admindetail
+		var joinDate time.Time
+		if err := rows.Scan(&admin.Email, &joinDate); err != nil {
+			log.Printf("Error scanning row: %v", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		admin.JoinDate = joinDate.Format("02-01-2006")
+		admins = append(admins, admin)
+	}
+	if err := rows.Err(); err != nil {
+		log.Printf("Error iterating over rows: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(admins); err != nil {
+		log.Printf("Error encoding response: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
